4155b922f808add39e86c9dd30397e4b968b49c1
Patryk <dev@pwesolowski.pl>
2020-05-24 19:25:20 +0200

    add service worker

--- src/components/Pages/HomePage/components/HomePageComponent.jsx
+++ src/components/Pages/HomePage/components/HomePageComponent.jsx
@@ -4,7 +4,7 @@ import { StyledHomePageComponent } from "./HomePageComponent.styled";
 export default function HomePageComponent() {
   return (
     <StyledHomePageComponent>
-      <h2>Home</h2>
+      <h2>Panel</h2>
       <p>
         Witaj w panelu użytkownika. Aby przejść do instrukcji obsługi aplikacji{" "}
         <a href="https://github.com/wesbitpl/biwapk" target="_blank" rel="noopener noreferrer">
