1e46598b515e4bd0bcc0b9dd3047f1e759389daf
Patryk <dev@pwesolowski.pl>
2020-05-28 23:34:00 +0200

    add normalize.css

--- src/theme/global.jsx
+++ src/theme/global.jsx
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 
 export const GlobalStyles = createGlobalStyle`
+${normalize}
+
 html, body{
     margin:0;
     padding:0;
@@ -13,5 +16,7 @@ body{
     color: ${({ theme }) => theme.color.black};
     font-family: "Montserrat", sans-serif;
     text-rendering: optimizeLegibility;
+    width:100%;
+    min-height:100vh;
 }
 `;
