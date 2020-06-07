9bc6a984c524a731e73fb8a6cc7212651be3f6de
Patryk <dev@pwesolowski.pl>
2020-06-07 18:56:09 +0200

    fix condition

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,7 +1,7 @@
 export const url = () => {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:1337";
-  } else {
+  } else if (process.env.NODE_ENV === "production") {
     return "https://biwapk.herokuapp.com";
   }
 };
